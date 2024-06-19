a4079b7e196640be06c6eaeb0681923dcee2e0f4
Hunter Sharpe <huntersharpe@me.com>
2024-06-19 00:42:24 -0500

    Searching, turned sov sync back on, favicon and bug fixes

---
+++ EquinoxResourceBrowser/Interfaces/ISearchService.cs
@@ -0,0 +1,9 @@
+﻿using EquinoxResourceBrowser.Dtos;
+
+namespace EquinoxResourceBrowser.Interfaces
+{
+    public interface ISearchService
+    {
+        Task<List<SearchResultDto>> LoadUniverse();
+    }
+}
